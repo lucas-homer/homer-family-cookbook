9ac32e70af38ab54f05f7b078928b8b1fdf051c8
lucas homer <lucas.homer@gmail.com>
2022-04-06 20:55:11 -0700

    😅

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -33,7 +33,7 @@ export default function Index() {
                     to="/notes"
                     className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-blue-700 shadow-sm hover:bg-blue-50 sm:px-8"
                   >
-                    View Notes for {user.email}
+                    View Notes - {user.email}
                   </Link>
                 ) : (
                   <div className="space-y-4 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5 sm:space-y-0">
