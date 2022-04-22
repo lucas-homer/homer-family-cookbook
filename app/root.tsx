fad53aeadfb0a8861be8287e60a497e58d8fea75
lucas homer <lucas.homer@gmail.com>
2022-04-22 14:59:47 -0700

    extract desktop menu into its own component

--- app/root.tsx
+++ app/root.tsx
@@ -5,11 +5,9 @@ import type {
 } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import {
-  Form,
   Links,
   LiveReload,
   Meta,
-  NavLink,
   Outlet,
   Scripts,
   ScrollRestoration,
@@ -20,6 +18,7 @@ import tailwindStylesheetUrl from "./styles/tailwind.css";
 import mobileMenuStylesheetUrl from "./styles/mobile-menu.css";
 import { getUser } from "./session.server";
 import MobileMenu from "./components/mobile-menu";
+import DesktopMenu from "./components/desktop-menu";
 
 export const links: LinksFunction = () => {
   return [
@@ -54,100 +53,10 @@ export default function App() {
         <Links />
       </head>
       <body className="flex h-full min-h-screen flex-col flex-nowrap md:flex-row">
-        <div>
-          <MobileMenu userLoggedIn={data.user ? true : false} />
-        </div>
-        <nav className="hidden h-full w-80 border-r bg-gray-50 md:block">
-          <ul>
-            <li>
-              <NavLink
-                className={({ isActive }) =>
-                  `block border-b p-4 text-xl ${isActive ? "bg-white" : ""}`
-                }
-                to="."
-                aria-label="Home"
-              >
-                🏠 Home
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                className={({ isActive }) =>
-                  `block border-b p-4 text-xl ${isActive ? "bg-white" : ""}`
-                }
-                to="search"
-              >
-                🔎 Search
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                className={({ isActive }) =>
-                  `block border-b p-4 text-xl ${isActive ? "bg-white" : ""}`
-                }
-                to="categories"
-              >
-                📚 Categories
-              </NavLink>
-            </li>
-            {data.user && (
-              <>
-                <li>
-                  <NavLink
-                    className={({ isActive }) =>
-                      `block border-b p-4 text-xl ${isActive ? "bg-white" : ""}`
-                    }
-                    to="favorites"
-                  >
-                    ❤️ Favorites
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink
-                    className={({ isActive }) =>
-                      `block border-b p-4 text-xl ${isActive ? "bg-white" : ""}`
-                    }
-                    to="recently-viewed"
-                  >
-                    ⏲ Recently Viewed
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink
-                    className={({ isActive }) =>
-                      `block border-b p-4 text-xl ${isActive ? "bg-white" : ""}`
-                    }
-                    to="recipes/new"
-                  >
-                    📝 Add Recipe
-                  </NavLink>
-                </li>
-                <li>
-                  <Form action="/logout" method="post">
-                    <button
-                      type="submit"
-                      className="block w-full border-b p-4 text-left text-xl"
-                    >
-                      🔒 Logout
-                    </button>
-                  </Form>
-                </li>
-              </>
-            )}
-            {!data.user ? (
-              <li>
-                <Form action="/login" method="post">
-                  <button
-                    type="submit"
-                    className="block w-full border-b  p-4 text-left text-xl"
-                  >
-                    🔓 Login
-                  </button>
-                </Form>
-              </li>
-            ) : null}
-          </ul>
-        </nav>
+        <header>
+          <MobileMenu userLoggedIn={Boolean(data.user)} />
+          <DesktopMenu userLoggedIn={Boolean(data.user)} />
+        </header>
 
         <main className="flex-1 p-6">
           <Outlet />
