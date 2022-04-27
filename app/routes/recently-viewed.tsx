5b3d5858445897d8f7d0c8622a57776851ee240b
lucas homer <lucas.homer@gmail.com>
2022-04-26 19:46:37 -0700

    build out recently viewed recipes page with sort and recipesummary components

--- app/routes/recently-viewed.tsx
+++ app/routes/recently-viewed.tsx
@@ -1,5 +1,7 @@
+import { ChevronDownIcon, ChevronUpIcon } from "@radix-ui/react-icons";
 import { json, LoaderFunction } from "@remix-run/node";
-import { Link, useLoaderData } from "@remix-run/react";
+import { Link, useLoaderData, useSearchParams } from "@remix-run/react";
+import RecipeSummary from "~/components/recipe-summary";
 import { getRecentlyViewed } from "~/models/user.server";
 import { requireUserId } from "~/session.server";
 
@@ -9,27 +11,55 @@ type LoaderData = {
 
 export const loader: LoaderFunction = async ({ request }) => {
   const userId = await requireUserId(request);
-  const recipes = await getRecentlyViewed(userId);
+  const url = new URL(request.url);
+  const sort = url.searchParams.get("sort") as "newest" | "oldest" | undefined;
+
+  const recipes = await getRecentlyViewed(userId, sort);
 
   return json<LoaderData>({ recipes });
 };
 
 export default function RecentlyViewed() {
   const { recipes } = useLoaderData() as LoaderData;
+
+  const [searchParams] = useSearchParams();
+  const sortDirection = searchParams?.get("sort") ?? "newest"; // default to newest first
+  const nextSort = sortDirection === "oldest" ? "newest" : "oldest";
+
   return (
     <div className="relative min-h-screen bg-white ">
-      <h2 className="mb-8 text-2xl">Recently Viewed Recipes</h2>
+      <h2 className="mb-4 text-2xl md:mb-24">Recently Viewed Recipes</h2>
       <ul className="mb-4">
         {recipes.length ? (
-          recipes.map((item) => (
-            <li key={item.recipe.id} className="mb-2 text-xl">
-              <h2 className="text-xl">
-                <Link to={`/recipes/${item.recipeId}`}>
-                  {item.recipe.title}
-                </Link>
-              </h2>
-            </li>
-          ))
+          <>
+            <Link
+              to={`?sort=${nextSort}`}
+              className="mb-2 flex max-w-xs items-center gap-1"
+            >
+              {sortDirection === "newest" ? (
+                <>
+                  <ChevronDownIcon className="" />
+                  <span>Sort: Newest</span>
+                </>
+              ) : (
+                <>
+                  <ChevronUpIcon className="" />
+                  <span>Sort: Oldest</span>
+                </>
+              )}
+            </Link>
+
+            <ul>
+              {recipes.map((item) => (
+                <li key={item.recipeId} className="mb-4">
+                  <RecipeSummary
+                    recipe={item.recipe}
+                    lastViewed={item.updatedAt}
+                  />
+                </li>
+              ))}
+            </ul>
+          </>
         ) : (
           <p>You don't have any recently viewed recipes yet!</p>
         )}
