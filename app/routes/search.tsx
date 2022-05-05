5879a1ae0e0cbe7234b706545ec476cff326c473
lucas homer <lucas.homer@gmail.com>
2022-05-05 16:39:54 -0700

    hook up autocomplete ui to the index

--- app/routes/search.tsx
+++ app/routes/search.tsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from "@remix-run/react";
+import { useNavigate } from "@remix-run/react";
 import algoliasearch from "algoliasearch/lite";
 import { getAlgoliaResults } from "@algolia/autocomplete-js";
 import { Autocomplete } from "~/components/autocomplete";
@@ -19,7 +19,10 @@ export const links = () => {
   ];
 };
 
-const searchClient = algoliasearch("", "");
+const searchClient = algoliasearch(
+  "HW88ALT84E",
+  "42af7b48a35f7754841b7c3bd0210b44"
+);
 
 export default function Search() {
   const navigate = useNavigate();
@@ -35,25 +38,89 @@ export default function Search() {
       id="searchModal"
       className="rounded-lg"
     >
-      <div>
-        <div className="mb-2 flex items-baseline justify-between">
-          <h2 className="text-xl">Search the Site</h2>
-          <button onClick={onDismiss} className="flex flex-nowrap items-center">
-            <span className="mr-1 pb-0.5">esc</span>
-            <Cross2Icon width={24} height={24} />
-          </button>
-        </div>
-        <p className="mb-4">
-          You can search for recipes, categories, or ingredients.
-        </p>
-
-        <Autocomplete
-          placeholder={"Search"}
-          getSources={() => {
-            return [];
-          }}
-        />
+      <div className="mb-2 flex items-baseline justify-between">
+        <h2 className="text-xl">Search the Site</h2>
+        <button onClick={onDismiss} className="flex flex-nowrap items-center">
+          <span className="mr-1 pb-0.5">esc</span>
+          <Cross2Icon width={24} height={24} />
+        </button>
       </div>
+      <p className="mb-4">
+        You can search for recipes, categories, or ingredients.
+      </p>
+
+      <Autocomplete
+        placeholder={"Search"}
+        getSources={({ query }: { query: any }) => {
+          return [
+            {
+              sourceId: "recipes",
+              getItemUrl({ item }: { item: any }) {
+                return `http://localhost:3000/recipes/${item.id}`;
+              },
+              getItems() {
+                return getAlgoliaResults({
+                  searchClient,
+                  queries: [
+                    {
+                      indexName: "dev_homerfamilycookbook",
+                      query,
+                      params: {
+                        hitsPerPage: 5,
+                      },
+                    },
+                  ],
+                });
+              },
+              templates: {
+                item({ item, components }: { item: any; components: any }) {
+                  return (
+                    <a className="aa-ItemLink" href={`/recipes/${item.id}`}>
+                      <div className="aa-ItemContent">
+                        <div className="aa-ItemContentBody">
+                          <div className="aa-ItemContentTitle">
+                            <components.Highlight
+                              hit={item}
+                              attribute="title"
+                            />
+                          </div>
+                          <div className="aa-ItemContentDescription">
+                            {/* TODO -- figure out how to show categories and ingredients */}
+                            {/* <components.Snippet
+                                hit={item}
+                                attribute="title"
+                              />
+                              <components.Snippet
+                                hit={item}
+                                attribute="ingredients[]"
+                              /> */}
+                          </div>
+                        </div>
+                      </div>
+                      <div className="aa-ItemActions">
+                        <button
+                          className="aa-ItemActionButton aa-DesktopOnly aa-ActiveOnly"
+                          type="button"
+                          title="Select"
+                        >
+                          <svg
+                            viewBox="0 0 24 24"
+                            width="20"
+                            height="20"
+                            fill="currentColor"
+                          >
+                            <path d="M18.984 6.984h2.016v6h-15.188l3.609 3.609-1.406 1.406-6-6 6-6 1.406 1.406-3.609 3.609h13.172v-4.031z" />
+                          </svg>
+                        </button>
+                      </div>
+                    </a>
+                  );
+                },
+              },
+            },
+          ];
+        }}
+      />
     </Dialog>
   );
 }
