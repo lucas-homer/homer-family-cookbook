65618ca32ff89a0bb4df136ad6c1a92d65a455ea
lucas homer <lucas.homer@gmail.com>
2022-05-06 11:25:41 -0700

    i dont fully understand what is happening

--- app/routes/search.tsx
+++ app/routes/search.tsx
@@ -51,7 +51,7 @@ export default function Search() {
       </p>
 
       <Autocomplete
-        detachedMediaQuery="none"
+        // detachedMediaQuery="none"
         placeholder={"Search"}
         getSources={({ query }: { query: any }) => {
           return [
