cd4b323885da26b18da1689e568173c177bd8dc7
lucas homer <lucas.homer@gmail.com>
2022-05-09 20:15:43 -0700

    add skipnav

--- app/root.tsx
+++ app/root.tsx
@@ -13,6 +13,7 @@ import {
   ScrollRestoration,
   useLoaderData,
 } from "@remix-run/react";
+import skipNavStyles from "@reach/skip-nav/styles.css";
 import tailwindStylesheetUrl from "./styles/tailwind.css";
 import globalStylesheetUrl from "./styles/globals.css";
 import mobileMenuStylesheetUrl from "./styles/mobile-menu.css";
@@ -20,6 +21,7 @@ import { getUser } from "./session.server";
 import MobileMenu from "./components/mobile-menu";
 import DesktopMenu from "./components/desktop-menu";
 import SearchButton from "./components/search-button";
+import { SkipNavContent, SkipNavLink } from "@reach/skip-nav";
 
 export const links: LinksFunction = () => {
   return [
@@ -55,6 +57,10 @@ export const links: LinksFunction = () => {
     { rel: "stylesheet", href: tailwindStylesheetUrl },
     { rel: "stylesheet", href: mobileMenuStylesheetUrl },
     {
+      rel: "stylesheet",
+      href: skipNavStyles,
+    },
+    {
       rel: "icon",
       type: "image/x-icon",
       href: "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🍪</text></svg>",
@@ -88,6 +94,8 @@ export default function App() {
         <Links />
       </head>
       <body className="flex h-full min-h-screen flex-col flex-nowrap md:flex-row">
+        <SkipNavLink />
+
         <header className="md:h-full md:min-h-screen md:overflow-y-auto">
           <MobileMenu userLoggedIn={Boolean(data.user)} />
           <SearchButton />
@@ -95,7 +103,9 @@ export default function App() {
         </header>
 
         <main className="flex-1 overflow-y-auto p-6 md:px-8">
-          <Outlet />
+          <SkipNavContent>
+            <Outlet />
+          </SkipNavContent>
         </main>
 
         <ScrollRestoration />
