a59bc17da052bbaa4037be1d8e8f34e6bdb60235
lucas homer <lucas.homer@gmail.com>
2022-05-11 20:11:09 -0700

    add favicons for all device types

--- app/root.tsx
+++ app/root.tsx
@@ -61,10 +61,24 @@ export const links: LinksFunction = () => {
       href: skipNavStyles,
     },
     {
-      rel: "icon",
-      type: "image/x-icon",
-      href: "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🍪</text></svg>",
+      rel: "apple-touch-icon",
+      sizes: "180x180",
+      href: "/favicons/apple-touch-icon.png",
     },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      href: "/favicons/favicon-32x32.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "16x16",
+      href: "/favicons/favicon-16x16.png",
+    },
+    { rel: "manifest", href: "/site.webmanifest" },
+    { rel: "icon", href: "/favicon.ico" },
   ];
 };
 
